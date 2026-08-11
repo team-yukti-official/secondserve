36150440ed5b6bbe5061652a7b14597598090d77
team-yukti-official <team.yukti.official@gmail.com>
2026-08-11 19:21:47 +0530

    Update env-config.js

--- script/env-config.js
+++ script/env-config.js
@@ -9,6 +9,6 @@
 
 window.ENV_CONFIG = {
   API_BASE_URL: 'https://openrouter.ai/api/v1/chat/completions',
-  API_KEY: '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',  // Add your real API key
-  AI_MODEL_NAME: 'google/gemma-4-26b-a4b-it:free'
+  API_KEY: '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',  // Add your real API key
+  AI_MODEL_NAME: 'stepfun/step-3.7-flash'
 };
