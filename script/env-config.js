e9b643a9e31a74595cae4559e5f9746dab77a2b3
team-yukti-official <team.yukti.official@gmail.com>
2026-08-22 03:08:30 +0530

    Update env-config.js

--- script/env-config.js
+++ script/env-config.js
@@ -1,5 +1,5 @@
 window.ENV_CONFIG = {
   API_BASE_URL: 'https://openrouter.ai/api/v1/chat/completions',
-  API_KEY: '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',  // Add your real API key
-  AI_MODEL_NAME: 'nvidia/nemotron-nano-12b-v2-vl:free'
+  API_KEY: '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',  // Add your real API key
+  AI_MODEL_NAME: 'google/gemma-4-31b-it'
 };
